9dec723bfafe605a7bbd2d2e3b8bda5d9b09ccaf
Pollux1989 <145251089+Pollux1989@users.noreply.github.com>
2024-04-30 09:47:15 +1000

    initials.py

---
+++ Hello World/initials.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+print('JJJJJ  RRRR ')
+print('  J    R   R')
+print('  J    R   R')
+print('  J    RRRR ')
+print('J J    R R  ')
+print('J J    R  R ')
+print(' JJ    R   R')
+
+
