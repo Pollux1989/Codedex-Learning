7ba129a2d4bf89494f414dc062a389273c087b74
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-04-30 14:00:57 +1000

    Create 07. Temperat.py

---
+++ Variables/07. Temperat.py
@@ -0,0 +1,30 @@
+# Write code below 💖
+
+score = 0           # score is 0
+score = 4 + 3       # score is now 7
+score = 4 - 3       # score is now 1
+score = 4 * 3       # score is now 12
+score = 4 / 3       # score is now 1.3333
+score = 4 % 3       # score is now 1
+
+print(score)        # Output: 1
+
+pizza = 2.99
+coke = 0.99
+
+
+tip = (pizza + coke) * 0.2
+print(tip)  
+
+##### Temperat########
+#Create a temperature.py program that converts a temperature from Fahrenheit (°F) to Celsius (°C).
+#Google the current temperature of Brooklyn, NY (where Codédex is based) in °F.
+
+
+fahrenheit = 59
+
+celsius = (fahrenheit - 32)/ 1.8
+print(celsius)
+
+
+
