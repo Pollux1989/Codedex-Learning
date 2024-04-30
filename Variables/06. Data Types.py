9df726a9536e5fc4abbc4c2cae55e872ce548a86
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-04-30 13:42:05 +1000

    Update 06. Data Types.py

--- Variables/06. Data Types.py
+++ Variables/06. Data Types.py
@@ -1,1 +1,31 @@
+# Write code below 💖
+#Variables
+name = 'Jair Robles'
+user_id = 132456798
+progress = 0.75
+exp = 60
+verified = True
+xp = 70
+xp = 80
+
+# Tipos de datos
+
+## Entero -> int
+year = 2024
+age = 34
+
+## Flotar -> float
+pi = 3.14159
+meal_cost = 12.99
+
+## Cadena->str
+message = "polvora"
+user = '@Labmedia'
+
+## booleano
+late_to_class = False
+cranky = True
+
+print(xp)    # Output: 80
+
 
