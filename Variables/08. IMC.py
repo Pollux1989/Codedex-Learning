09c19cdb8fedfda0bdbd22c3a093660f8157deae
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-04-30 14:09:52 +1000

    Update and rename 08. IMC to 08. IMC.py

---
+++ Variables/08. IMC.py
@@ -0,0 +1,27 @@
+# Write code below 💖
+# Exponents
+
+score = 2 ** 2      # score is 4
+score = 2 ** 3      # score is now 8
+score = 2 ** 4      # score is now 16
+score = 2 ** 5      # score is now 32
+
+print(score)        # Output: 32
+
+
+# 08. BMI:
+## The body mass index (BMI) was created by a Belgian mathematician in the 1850s 
+## and it's used by health and nutrition professionals to estimate human body fat in certain populations.
+## It is computed by taking an individual's weight (mass) 
+## in kilograms and dividing it by the square of their height in meters.
+
+mass = 75
+height = 1.82
+
+bmi = mass / (height ** 2)
+
+print(bmi)
+
+
+
+
