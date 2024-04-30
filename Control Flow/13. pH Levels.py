1a2d46ec23691c2c927ce91b719f7d5176226410
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-04-30 21:54:24 +1000

    Create 13. pH Levels.py

---
+++ Control Flow/13. pH Levels.py
@@ -0,0 +1,11 @@
+# 13. pH Levels
+# Write code below 💖
+
+ph = int(input('Type here the PH value between 0 - 14: '))
+
+if ph > 7:
+  print('PH Basic')
+elif ph < 7:
+  print('PH Acid')  
+else:
+  print('Neutral')  
