a75770bb8e0afb247b5acadda04974c06f6315f1
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-04-30 16:23:03 +1000

    Create 10. Currency.py

---
+++ Variables/10. Currency.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+co = int(input('What do you have left in pesos?: '))
+pe = int(input('What do you have left in soles?: '))
+br = int(input('What do you have left in reais?: '))
+
+co_aud = co * 0.00040
+pe_aud = pe * 0.41
+br_aud = br * 0.30
+
+print(co_aud + pe_aud + br_aud)
+
+
+
