ec6c28150706235fd69a011853fc7885335919a0
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-05-01 16:04:39 +1000

    Create 17. Enter PIN.py

---
+++ Loops/17. Enter PIN.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+print(' Bancolombia')
+
+pin = int(input('Ingrese su Pin: '))
+
+while pin != 1234:
+  pin = int(input('Pin Incorrecto. Ingrese su PIN nuevamente: '))
+
+if pin == 1234:
+  print('Pin Correcto!')
