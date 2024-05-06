73d541a06deb37bcac887da68cd2c01a5f63c4a3
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-05-06 11:52:34 +1000

    Create 27. Bucket List.py

---
+++ Lists/27. Bucket List.py
@@ -0,0 +1,18 @@
+## 27. Bucket List
+
+things_to_do = [
+  '🚀 Create the dopest learn to code platform ever.',
+  '⛰️ Visit himalayas.',
+  '🏡 Buy my own place.',
+  '🌏 Live somewhere in Asia for a year.',
+  '🎸 Larn to play a Guitar.',
+  '📝 Write a book.',
+  '🏆 Reach my PR.',
+  '🚐 Road trip with the fam.',
+  '🍳 Open a cozy diner upstate.',
+  '👴🏻 Grow old with no regrets.'
+]
+
+for i in things_to_do:
+  print(i)
+  
