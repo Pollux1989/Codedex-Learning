e1c9f751f710439b711057bcafeed6bc43002078
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-05-06 10:47:12 +1000

    Create 23. To-Do List.py

---
+++ Lists/23. To-Do List.py
@@ -0,0 +1,18 @@
+## 23. To-Do List
+
+"""
+'🏦 Get quarters.'
+'🧺 Do laundry.'
+'🌳 Take a walk.'
+'💈 Get a haircut.'
+'🍵 Make some tea.'
+'💻 Complete Lists chapter.'
+'💖 Call mom.'
+'📺 Watch My Hero Academia.'
+"""
+
+to_do_list = ['Get quarters', 'Do Lundry', 'Take a Walk', 'Get a Haircut', 'Make Some Tea', 'Complete Lists Chapter','Call Mom', 'Watch My Hero Academia']
+print(to_do_list[0])
+print(to_do_list[1])
+print(to_do_list[2:5])
+print(to_do_list[9])
