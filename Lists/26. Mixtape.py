be3ac0c5b414a59bbef5d934bb9c5b00a4800108
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-05-06 11:37:38 +1000

    Create 26. Mixtape.py

---
+++ Lists/26. Mixtape.py
@@ -0,0 +1,24 @@
+## 26. Mixtape
+
+"""
+Back in the good ol' days, a mixtape is a compilation
+of music, a playlist, recorded onto a cassette tape or a CD.
+"""
+
+# 💿 Theme: Rock 2000 - Travel Songs
+
+playlist = [
+  'Linking Park - Numb',
+  'SOAD - Shop Suay',
+  'Foo Figthers - Everlong',
+  'RAM - Killing in the Name',
+  'Papa Roach - Last Resort',
+  'Slipknot - Vermilion'
+]
+for i in playlist:
+  print(i)
+  
+for i in range(len(playlist)):
+  print(playlist[i])
+  
+
