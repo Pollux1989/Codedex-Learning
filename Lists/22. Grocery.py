e23a9aa0c1bc30f5ece15f362e618c1641a06393
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-05-06 10:29:08 +1000

    Create 22. Grocery.py

---
+++ Lists/22. Grocery.py
@@ -0,0 +1,16 @@
+# Write code below 💖
+# 22. Grocery List
+
+"""'
+🥚 Eggs'
+'🥑 Avocados'
+'🍪 Cookies'
+'🌶 Hot Pepper Jam'
+'🫐 Blueberries'
+'🥦 Broccoli'
+"""
+
+grocery = ['Eggs','Avocados', 'Cookies', 'Hot Pepper Jam', 'Blueberries', 'Brocoli']
+
+
+print(grocery)
