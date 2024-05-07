91af74772b845e4033c4585ee9163b02223fa817
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-05-07 21:08:25 +1000

    Update 30. Mars Orbiter.py

--- Functions/30. Mars Orbiter.py
+++ Functions/30. Mars Orbiter.py
@@ -1,1 +1,12 @@
 
+## 30. Mars Orbiter
+
+print("#####--------30. Mars Orbiter--------#####")
+
+distance = float(input('Type here the distance in KM: '))
+
+def distance_to_miles(distance):
+    print(distance / 1.609)
+
+distance_to_miles(distance)
+
